b28e9ec84280fe668a1aa601d2caf7900096022f
Shaggy8 <rominus@bigmir.net>
2016-02-29 00:07:28 +0200

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,27 @@ jQuery(function () {
 				wrap: 'both',
 				center: true,
 				animation: 'slow',
-		});
+	});
 
-	$('.jcarousel-prev').jcarouselControl({
-				target: '-=1'
+		$('.jcarousel-prev').jcarouselControl({
+					target: '-=1'
 		});
 
 		$('.jcarousel-next').jcarouselControl({
 				target: '+=1'
 		});
+
+		$('.jcarousel-pagination').jcarouselPagination({
+				item: function(page) {
+						return '<a href="#' + page + '">' + page + '</a>';
+				}
+		});
+
+		var $jcarouselPagination = $('.jcarousel-pagination');
+		var jcarPaginWidth = $jcarouselPagination.outerWidth();
+		var jcarWidth = $jcarouselPagination.parent().width();
+
+		$jcarouselPagination.css({
+			'left': jcarWidth / 2 - jcarPaginWidth / 2,
+		})
 })
