c2d917e0f381b185c392c1c3ebe8cd9beb6f97c4
Shaggy8 <rominus@bigmir.net>
2016-03-02 01:50:23 +0200

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

--- js/scripts.js
+++ js/scripts.js
@@ -22,68 +22,70 @@ jQuery(function () {
 
 
 
-	$('.jcarousel').jcarousel({
-			wrap: 'both',
-			center: true,
-			animation: 'slow',
-	});
+	$('img').load(function() {
+		$('.jcarousel').jcarousel({
+				wrap: 'both',
+				center: true,
+				animation: 'slow',
+		});
 
 
-	$('.jcarousel-prev').jcarouselControl({
-			target: '-=1'
-	});
+		$('.jcarousel-prev').jcarouselControl({
+				target: '-=1'
+		});
 
 
-	$('.jcarousel-next').jcarouselControl({
-			target: '+=1'
-	});
+		$('.jcarousel-next').jcarouselControl({
+				target: '+=1'
+		});
 
 
-	$('.jcarousel-pagination').jcarouselPagination({
-			item: function(page) {
-				return '<a href="#' + page + '">' + page + '</a>';
+		$('.jcarousel-pagination').jcarouselPagination({
+				item: function(page) {
+					return '<a href="#' + page + '">' + page + '</a>';
+				}
+		});
+
+
+		var jcarWidth = {
+			jcar: $('.jcarousel').width(),
+			pagin: $('.jcarousel-pagination').outerWidth(),
+			};
+
+		$('.jcarousel-pagination').css({
+			'left': jcarWidth.jcar / 2 - jcarWidth.pagin / 2,
+		});
+		$('.jcarousel-pagination a:first-child').css({
+			'background': 'yellow',
+		});
+		
+
+		$('.jcarousel').on('click.color', function(event) {
+			var $target = $(event.target);
+			var $pages = $('.jcarousel-pagination a');
+
+			if ($target.is('.jcarousel-pagination a')) {
+				$pages.css({'background': ''});
+				$target.css({'background': 'yellow'});
+				return;
 			}
-	});
 
+			var page = - parseFloat($('.jcarousel-list').css('left')) /
+				$('.jcarousel').width();
 
-	var jcarWidth = {
-		jcar: $('.jcarousel').width(),
-		pagin: $('.jcarousel-pagination').outerWidth(),
-		};
+			if ($target.is('.jcarousel-prev')) {
+				$pages.css({'background': ''});
+				var getPage = $pages.get(page - 1);
+				$(getPage).css({'background': 'yellow'});
+			};
 
-	$('.jcarousel-pagination').css({
-		'left': jcarWidth.jcar / 2 - jcarWidth.pagin / 2,
-	});
-	$('.jcarousel-pagination a:first-child').css({
-		'background': 'yellow',
-	});
-	
-
-	$('.jcarousel').on('click.color', function(event) {
-		var $target = $(event.target);
-		var $pages = $('.jcarousel-pagination a');
-
-		if ($target.is('.jcarousel-pagination a')) {
-			$pages.css({'background': ''});
-			$target.css({'background': 'yellow'});
-			return;
-		}
-
-		var page = - parseFloat($('.jcarousel-list').css('left')) /
-			$('.jcarousel').width();
-
-		if ($target.is('.jcarousel-prev')) {
-			$pages.css({'background': ''});
-			var getPage = $pages.get(page - 1);
-			$(getPage).css({'background': 'yellow'});
-		};
-
-		if ($target.is('.jcarousel-next')) {
-			if ((page + 1) == $pages.length) page = - 1;
-			$pages.css({'background': ''});
-			var getPage = $pages.get(page + 1);
-			$(getPage).css({'background': 'yellow'});
-		};
+			if ($target.is('.jcarousel-next')) {
+				if ((page + 1) == $pages.length) page = - 1;
+				$pages.css({'background': ''});
+				var getPage = $pages.get(page + 1);
+				$(getPage).css({'background': 'yellow'});
+			};
+		});
 	});
 
 
